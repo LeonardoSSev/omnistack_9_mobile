3776da1e1ca8e08730ad278a7f14ee066abd03d4
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-28 02:13:37 -0300

    Lists techs

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, SafeAreaView, AsyncStorage, Image, StyleSheet } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-  return <View/>
-}
+  const [techs, setTechs] = useState('');
+useEffect(() => {
+  AsyncStorage.getItem('techs').then(storageTechs => {
+    const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+    setTechs(techsArray);
+  })
+}, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} style={styles.logo}></Image>
+      <Text>{techs}</Text>
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 37,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10
+  }
+})
