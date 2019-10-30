c882c0ab425fe7a9c5ad940ca09dc5fb0415fdd0
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-30 11:06:47 -0300

    Ignores warnings about web socket

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Routes from './src/routes';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized Web Socket'
+]);
 
 export default function App() {
   return <Routes/>
