fcfe2b227d93dfd37dd703e44ee5cd5b1c97c051
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-30 11:40:26 -0300

    Shows alert when listening booking_response from web socket

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, SafeAreaView, AsyncStorage, Image, StyleSheet } from 'react-native';
+import { Alert, ScrollView, SafeAreaView, AsyncStorage, Image, StyleSheet } from 'react-native';
 import SpotList from '../components/SpotList';
 import logo from '../assets/logo.png';
+import socketio from 'socket.io-client';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
-useEffect(() => {
-  AsyncStorage.getItem('techs').then(storageTechs => {
-    const techsArray = storageTechs.split(',').map(tech => tech.trim());
 
-    setTechs(techsArray);
-  })
-}, []);
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.15.39:3333', {
+        query: { user_id }
+      })
+
+      socket.on('booking_response', booking => {
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'RECUSADA'}`);
+      })
+    });
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storageTechs => {
+      const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    })
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
